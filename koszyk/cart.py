27bc5e9073fdeec801a207a05987d7219ad01bc8
CKaiserM <kaiser.mariusz@gmail.com>
2025-03-03 23:36:54 +0100

    cart functionality added. (add, update, delete items)

--- koszyk/cart.py
+++ koszyk/cart.py
@@ -14,14 +14,66 @@ class Cart():
         # make cart available on all pages
         self.cart = cart
     
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(1)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'cena': str(product.price)}
+            self.cart[product_id] = int(product_qty)
         
         self.session.modified = True
+
+    def update(self, product, quantity):
+        
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        cart = self.cart
+
+        cart[product_id] = product_qty
+
+        self.session.modified = True
+
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
+    def get_cart_total(self):
+        product_ids = self.cart.keys()
+        #filter cart products by ids.
+        products_in_carts = Product.objects.filter(id__in=product_ids)
+        cart_qts = self.cart
+
+        total = 0
+
+        for key, value in cart_qts.items():
+            key = int(key)
+            for product in products_in_carts:
+                if product.id == key:
+                    total = total + (product.price * value)
+        return total
+    
+    def get_cart_subtotal(self):
+        product_ids = self.cart.keys()
+        #filter cart products by ids.
+        products_in_carts = Product.objects.filter(id__in=product_ids)
+        #get cart items and qts
+        cart_qts = self.cart
+        #copy cart items
+        subtotal = cart_qts.copy()
+        
+        #
+        for key, value in subtotal.items():
+            key = int(key)
+            for product in products_in_carts:
+                if product.id == key:
+                    # multiply product price by qty and modify dict.
+                    subtotal[str(key)] = (value * product.price)
+        return subtotal
 
     def __len__(self):
         return len(self.cart)
@@ -32,3 +84,17 @@ class Cart():
         products_in_cart = Product.objects.filter(id__in=product_ids)
         #return all
         return products_in_cart
+    
+    def get_quantity(self):
+        quantity = self.cart
+        return quantity
+    
+    
+
+
+
+
+    
+
+    
+    
--- koszyk/views.py
+++ koszyk/views.py
@@ -19,29 +19,58 @@ class KoszykView(APIView):
     #get Cart summary
     def get(self, request):
         cart = Cart(request)
+        
         cart_summary = cart.get_products
-        return Response({'cart_summary':cart_summary})
+        cart_quantities = cart.get_quantity
+        cart_total = cart.get_cart_total
+        cart_subtotal = cart.get_cart_subtotal
+        return Response({'cart_summary':cart_summary, 'cart_quantities':cart_quantities, 'cart_total':cart_total, 'cart_subtotal':cart_subtotal})
         
     def add(request):
         cart = Cart(request)
 
         if request.POST.get('action') == 'post':
             product_id = int(request.POST.get('product_id'))
-            print(product_id)
+            
             product = get_object_or_404(Product, id=product_id)
 
 
             #Save session
-            cart.add(product=product)
-
+            cart.add(product=product, quantity=1)
             cart_quantity = cart.__len__()
 
             #return response
-            response = JsonResponse({'Nazwa produktu': product.title})
+            response = JsonResponse({'Nazwa produktu': product.title, "ilość":cart_quantity})
             
             return response
 
     def delete(request):
-        pass
+        cart = Cart(request)
+
+        if request.POST.get('action') == 'post':
+            product_id = int(request.POST.get('product_id'))
+            cart.delete(product=product_id)
+
+            #return response
+            response = JsonResponse({'Nazwa produktu': product_id})
+            
+            return response
+
+
     def update(request):
-        pass    
+        cart = Cart(request)
+
+        if request.POST.get('action') == 'post':
+            product_id = int(request.POST.get('product_id'))
+
+            product_qty = int(request.POST.get('product_qty'))
+
+            #Save session
+            cart.update(product=product_id, quantity=product_qty)
+
+            cart_quantity = cart.__len__()
+
+            #return response
+            response = JsonResponse({'Quantity': product_qty})
+            
+            return response
